fc7c74befd4e763e35b8a9a94a001a9c6416188f
Gushley110 <gusley_dominator@hotmail.com>
2018-10-01 20:32:05 -0500

    Modifying the structure to be Object Oriented

--- E2_socket_chat/socket_server.py
+++ E2_socket_chat/socket_server.py
@@ -1,9 +1,8 @@
 import socket
-import threading
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 host = socket.gethostname()
-port = 12219
+port = 12221
 
 exit_keyword = 'teamo'
 
@@ -12,32 +11,19 @@ s.bind((host, port))
 s.listen(5)
 c = None
 
-th_queue = list()
-c_queue = list()
-addr_queue = list()
-
-def response(s,c,addr):
-    print('Conectado desde', addr)
-    print('[Esperando respuesta...]')
-    print(c.recv(1024).decode())
-    q = input("Servidor:")
-    if q == exit_keyword:
-        s.close()
-        print('Ha terminado la conexion')
-        quit()
-    else:
-        c.send(q.encode())
-aux = 0
-
 while True:
-    print('[Esperando conexion...]')
-    c, addr = s.accept()
-    c_queue.append(c);
-    addr_queue.append(addr);
-    t = threading.Thread(target=response, name='nueva conexion' + str(aux), args=(s, c_queue[aux], addr[aux]))
-    t.start()
-    th_queue.append(t)
-    aux += 1
-    
-
+    if c is None:
+        print('[Esperando conexion...]')
+        c, addr = s.accept()
+        print('Conectado desde', addr)
+    else:
+        print('[Esperando respuesta...]')
+        print(c.recv(1024).decode())
+        q = input("Servidor: ")
+        if q == exit_keyword:
+            s.close()
+            print('Ha terminado la conexion')
+            quit()
+        else:
+            c.send(q.encode())
     
