39294205e1854e8fb03741d3222b18578a7c04b7
Gushley110 <gusley_dominator@hotmail.com>
2018-10-02 23:16:30 -0500

    Created a infinite loop to send and wait for responses

--- E2_socket_chat/socket_client.py
+++ E2_socket_chat/socket_client.py
@@ -2,11 +2,30 @@ import socket
 
 class ChatClient():
 
-	def __init__(self,port,host = 'localhost', kewyword = '¬'):
+	def __init__(self,port,host = 'localhost', keyword = '¬'):
 		self.port = port
 		self.host = host
-		self.kewyword = kewyword
+		self.keyword = keyword
 		self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		self.s.connect((self.host,self.port))
 
-	def send_message()
+	def send_message(self,msg):
+		if(msg == self.keyword):
+			self.s.close()
+			print('Conexión interrumpida por cliente')
+			quit()
+		else:
+			self.s.send(msg.encode())
+
+if __name__ == '__main__':
+
+	PORT = 12220
+	client = ChatClient(PORT)
+
+	while True:
+	
+		msg = input('... ')
+		client.send_message(msg)
+
+		ans = cliente.s.recv(1024)
+		print('Servidor: ',ans.decode())
