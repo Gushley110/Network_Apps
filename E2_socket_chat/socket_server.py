2358286efdcb6668d420b5e1343163be8b1cf24c
Gushley110 <gusley_dominator@hotmail.com>
2018-10-02 21:09:07 -0500

    Finished OO Structure

--- E2_socket_chat/socket_server.py
+++ E2_socket_chat/socket_server.py
@@ -1,4 +1,37 @@
-import socket
+import socket, threading
+
+class ChatServer():
+
+    def __init__(self, port, host = 'localhost', keyword = '¬'):
+        self.port = port
+        self.host = host
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.users = list()
+
+        try:
+            self.s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            self.s.bind((self.host, self.port))
+        except socket.error:
+            print('Bind failed %s' % (socket.error))
+            sys.exit()
+
+        self.s.listen(5)
+
+    def response(self, conn, addr):
+        print('Client connected with ' + addr[0] + ':' + str(addr[1]))
+        while True:
+            data = conn.recv(1024)
+            print(data.decode())
+            conn.sendall(reply) 
+             
+        conn.close() # Close
+
+    def accept_connection(self):
+        print('[Esperando conexión ...]')
+
+        while True
+            conn,addr = self.s.accept()
+            threading.Thread(target=self.response, args=(conn, addr)).start()
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 host = socket.gethostname()
@@ -6,6 +39,7 @@ port = 12221
 
 exit_keyword = 'teamo'
 
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 s.bind((host, port))
 
 s.listen(5)
@@ -19,9 +53,11 @@ while True:
     else:
         print('[Esperando respuesta...]')
         print(c.recv(1024).decode())
-        q = input("Servidor: ")
-        if q == exit_keyword:
+        msg = input('... ')
+        q = 'Servidor: ' + msg
+        if msg == exit_keyword:
             s.close()
+            c.close()
             print('Ha terminado la conexion')
             quit()
         else:
